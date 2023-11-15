0b1f5e70dfda1384b5e258adb85939ac3b74e31a
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-11-15 12:04:38 +0100

    oops update

--- js/lobby.js
+++ js/lobby.js
@@ -3,14 +3,19 @@
 function submitPassword () {
     const password = document.getElementById("lobbycode").value;
     console.log(password);
-    joinLame(password);
+    joinLobby(password);
 }
 
 function joinLobby (password) {
     fetching(`api/game.php?game=${password}`, "GET");
 }   
 
+function submitLobby () {
+    const hostName = document.getElementById("hostName").value;
+    console.log(hostName);
 
-function createLobby {
+}
+
+function createLobby (){
 
 }
