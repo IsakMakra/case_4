b8a52069d261f7817ebccab4453dd60d2a9488ca
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-11-15 11:18:34 +0100

    fetch function

---
+++ js/functions.js
@@ -0,0 +1,25 @@
+"use strict";
+
+  
+async function fetching(url, type, info) {
+    try {
+        let response = await fetch(url, {
+            method: type,
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                info
+            }),
+        });
+
+        console.log(response);
+        let data = await response.json();
+        console.log(data);
+
+        if (!response.ok) {
+            console.log(`Oops! Something went wrong, we got this from the server <span>${data.message}</span>.`);
+        } else {
+            console.log(data);
+        }
+    } catch (err) {
+        console.log(`Error: ${err.message}`);
+    }}
