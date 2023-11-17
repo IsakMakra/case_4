778a562a3d4ad9bf9010d330cc656076dea35b2a
Isak Makra <89833141+IsakMakra@users.noreply.github.com>
2023-11-17 11:49:40 +0100

    added vote/winner in php

--- api/host.php
+++ api/host.php
@@ -2,11 +2,14 @@
     require_once "index.php";
 
     //POST - Next question
-    $required_keys_POST_host = ["next", "server_code", "host"];
+        //next: 1,
+        //server_code: xxxx,
+        //host: x
+    $required_keys_POST = ["next", "server_code", "host"];
     if($request_method == "POST")
     {
         //Checks if the POST-request has the correct body
-        if(count(array_intersect($required_keys_POST_host, array_keys($request_data))) == count($required_keys_POST_host)) 
+        if(count(array_intersect($required_keys_POST, array_keys($request_data))) == count($required_keys_POST)) 
         {
             $next = $request_data["next"];
             $host = $request_data["host"];
@@ -21,15 +24,82 @@
                     //Updates the games.json file with next question index. 
                     $json = json_encode($games, JSON_PRETTY_PRINT);
                     file_put_contents($games_file, $json);
+                    $message = ["message" => "Success."];
+                    send_JSON($message);
                 }
             }
 
+            $message = ["message" => "Error, wrong host username or server code."];
+            send_JSON($message, 404);
 
         }
+        else
+        {
+            $message = ["message" => "Error in POST-request."];
+            send_JSON($message, 422);
+        }
+    }
+
+    //PATCH - Choose winner
+        //winner: x,
+        //server_code: xxxx
+        //host: x
+    $required_keys_PATCH = ["winner", "server_code", "host"];
+    if($request_method == "PATCH") 
+    {
+        //Checks if the POST-request has the correct body
+        if(count(array_intersect($required_keys_PATCH, array_keys($request_data))) == count($required_keys_PATCH)) 
+        {
+            $winner = $request_data["winner"];
+            $server_code = $request_data["server_code"];
+            $host = $request_data["host"];
+
+            foreach($games as $index => $game)
+            {
+                if($host == $game["host"] && $server_code == $game["server_code"])
+                {
+                    foreach ($game['users'] as $index2 => $user) 
+                    {
+                        foreach ($game['current_votes'] as $index3 => $vote) 
+                        {
+                            if ($user['username'] == $vote['user'] && $vote['vote'] == $winner) 
+                            {
+                                $games[$index]["users"][$index2]["points"] += 1;
+                            }
+                        }
+                    }
+                    
+                    
+                
             
+        
+    
+
+
+
+
+
+                    //Updates the games.json file with next question index. 
+                    $json = json_encode($games, JSON_PRETTY_PRINT);
+                    file_put_contents($games_file, $json);
+                    $message = ["message" => "Success."];
+                    send_JSON($message);
+                }
+            }
+
+            $message = ["message" => "Error, wrong host username or server code."];
+            send_JSON($message, 404);
+
+        }
+        else
+        {
+            $message = ["message" => "Error in PATCH-request."];
+            send_JSON($message, 422);
+        }
     }
 
     //GET - Get game object
+        //?server_code=xxxx&host=x
     if($request_method == "GET") 
     {   
         //Checks if GET-request has the correct parameter
@@ -45,6 +115,14 @@
                     send_JSON($game);
                 }
             }
+
+            $message = ["message" => "Error, wrong host username or server code."];
+            send_JSON($message, 404);
+        }
+        else
+        {
+            $message = ["message" => "Error in GET-request."];
+            send_JSON($message, 422);
         }
     }
 ?>
--- api/user.php
+++ api/user.php
@@ -1,3 +1,80 @@
-//GET - Get quiz info
+<?php
+    require_once "index.php";
 
-//POST - Vote
+    //POST - Vote
+        //vote: "x",
+        //user: "Isak",
+        //server_code: "1975"
+    $required_keys_POST = ["vote", "server_code", "user"];
+    if($request_method == "POST")
+    {
+        //Checks if the POST-request has the correct body
+        if(count(array_intersect($required_keys_POST, array_keys($request_data))) == count($required_keys_POST)) 
+        {
+
+            $server_code = $request_data["server_code"];
+            $vote = $request_data["vote"];
+            $username = $request_data["user"];
+
+            foreach($games as $index => $game)
+            {
+                if($server_code == $game["server_code"])
+                {
+                    $data = 
+                    [
+                        "vote" => $vote,
+                        "user" => $username
+                    ];
+                    $games[$index]["current_votes"][] = $data;
+
+                    //Updates the games.json file with next question index. 
+                    $json = json_encode($games, JSON_PRETTY_PRINT);
+                    file_put_contents($games_file, $json);
+                    $message = ["message" => "Success."];
+                    send_JSON($message);
+                }
+            }
+
+            $message = ["message" => "Error, wrong host username or server code."];
+            send_JSON($message, 404);
+
+        }
+        else
+        {
+            $message = ["message" => "Error in POST-request."];
+            send_JSON($message, 422);
+        }
+    }
+
+    //GET - Get quiz
+        //?server_code=xxx
+    if($request_method == "GET") 
+    {   
+        //Checks if GET-request has the correct parameter
+        if(isset($_GET["server_code"])) 
+        {
+            $server_code = $_GET["server_code"];
+
+            foreach($games as $index => $game)
+            {
+                if($server_code == $game["server_code"])
+                {
+                    $data = 
+                    [
+                        "quiz" => $game["quiz"],
+                        "current_question_nr" => $game["current_question_nr"]
+                    ];
+                    send_JSON($data);
+                }
+            }
+
+            $message = ["message" => "Error, wrong server code."];
+            send_JSON($message, 404);
+        }
+        else
+        {
+            $message = ["message" => "Error in GET-request."];
+            send_JSON($message, 422);
+        }
+    }
+?>
