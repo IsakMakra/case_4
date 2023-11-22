911c4ed7fecbeef9bbefd599f84bde61c3c9e7dc
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-11-22 12:09:11 +0100

    Display users on playerpage

--- js/player.js
+++ js/player.js
@@ -11,8 +11,22 @@ async function startPlayerPage () {
         let response = await fetcha (`api/user.php?server_code=${password}`, "GET");
         console.log(response);
         let data = await response.json();
-        console.log(data.current_question_nr);
         let questionNumber = data.current_question_nr;
+        console.log(data);
+        //console.log(data.quiz[questionNumber].alternatives);
+
+        let userArray = data.quiz[questionNumber].alternatives;
+
+        // Iterate through userArray and update the content of <p> elements
+        userArray.forEach((user, index) => {
+            const userParagraph = document.getElementById(`user${index + 1}`);
+            if (userParagraph) {
+                userParagraph.textContent = ""; // Clear existing content
+                userParagraph.textContent = user; // Set new content            
+            }
+        });
+
+        document.getElementById("feedback").textContent = data.quiz[questionNumber].question;
 
         if (questionNumber === 0) {
             console.log("Not ready yet");
