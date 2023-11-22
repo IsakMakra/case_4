5cd6bfb71d3aca9eec62dc5cffd3f4e5551c394f
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-11-22 11:00:56 +0100

    Player Page now works. Fetches the question status every second.

--- js/player.js
+++ js/player.js
@@ -2,36 +2,26 @@
 
 const password = localStorage.getItem("password");
 
-function startPlayerPage () {
+async function startPlayerPage () {
 
     const interval = 1000; // 1000 milliseconds = 1 second
 
-    function fetchData(password) {
-        console.log("Before fetch");
-        fetch(`api/user.php?server_code=${password}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Fetch failed');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log("After fetch");
-                // Check the condition (more than 0 or not)
-                if (data.current_question_nr > 0) {
-                    console.log("yes");
-                } else {
-                    console.log("no");
-                }
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
+    async function fetchData() {
+
+        let response = await fetcha (`api/user.php?server_code=${password}`, "GET");
+        console.log(response);
+        let data = await response.json();
+        console.log(data.current_question_nr);
+        let questionNumber = data.current_question_nr;
+
+        if (questionNumber === 0) {
+            console.log("Not ready yet");
+        } else {
+            console.log("Game is active");
+        }
     }
     
     const intervalId = setInterval(fetchData, interval);
-
-
 }
 
 startPlayerPage();
