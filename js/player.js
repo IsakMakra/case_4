71151f378573d3eff8d69933964231ff1bd6a07a
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-11-22 14:16:14 +0100

    Voting on Player page is now possible

--- js/player.js
+++ js/player.js
@@ -1,8 +1,62 @@
 "use strict";
 
 const password = localStorage.getItem("password");
+const username = localStorage.getItem("name");
+
+//Create eventlistener
+    // Get the parent div
+    const parentDiv = document.getElementById("users");
+
+        // Loop through the children and add an event listener to each one
+        for (let i = 0; i < parentDiv.children.length; i++) {
+            const child = parentDiv.children[i];
+
+            // Add an event listener (e.g., click event)
+            child.addEventListener("click", voteForPlayer);
+        }
+
+        async function voteForPlayer (event) {
+
+            for (let i = 0; i < parentDiv.children.length; i++) {
+                const child = parentDiv.children[i];
+    
+                child.setAttribute("disabled", true)
+            }
+
+            console.log(event.target.textContent);
+            let votedPlayer = event.target.textContent;
+            
+            let response = await fetcha (`api/user.php?server_code=${password}`, "GET");
+            let data = await response.json();
+            console.log(data.users);
+            let users = data.users;
+
+            let foundUser = null;
+
+            users.forEach(user => {
+                if (user.username === votedPlayer) {
+                    console.log("10 points to slytherin!");
+                    foundUser = user;
+                }})
+
+            if (foundUser) {
+                let infoData = {
+                    server_code: password,
+                    vote: votedPlayer,
+                    user: username
+                };
+
+                let response2 = await fetcha(`api/user.php`, "POST", infoData);
+                let data2 = await response2.json();
+                console.log(data2);
+            } else {
+                console.log("oops");
+            }    
+        }
 
 async function startPlayerPage () {
+
+   // child.setAttribute("disabled", false);
 
     const interval = 1000; // 1000 milliseconds = 1 second
 
@@ -22,9 +76,12 @@ async function startPlayerPage () {
             const userParagraph = document.getElementById(`user${index + 1}`);
             if (userParagraph) {
                 userParagraph.textContent = ""; // Clear existing content
-                userParagraph.textContent = user; // Set new content            
+                userParagraph.textContent = user; // Set new content        
+                userParagraph.disabled = false; // Re-enable the button    
             }
         });
+
+ 
 
         document.getElementById("feedback").textContent = data.quiz[questionNumber].question;
 
@@ -37,6 +94,7 @@ async function startPlayerPage () {
     
     const intervalId = setInterval(fetchData, interval);
 }
+
 
 startPlayerPage();
 
