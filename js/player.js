6fb9a1fc4b6a3d02b1ad1f4af4553cbb72deccf4
Isak Makra <89833141+IsakMakra@users.noreply.github.com>
2023-11-29 18:57:55 +0100

    Added leaderboard to player.js

--- js/player.js
+++ js/player.js
@@ -6,8 +6,21 @@ const username = localStorage.getItem("name");
 document.querySelector("#namn").textContent = username;
 let watingForGameToStart = true;
 let intervalId;
+let leaderBoardIntervalId = 0;
 let timerIntervalId;
-let OldQuestionNumber = 0;
+let q_nr = 0;
+
+let timerStarted = false;
+let leaderBoardCreated = false;
+
+window.addEventListener('beforeunload', beforeUnloadHandler);
+
+function beforeUnloadHandler(event) {
+    event.preventDefault();
+    var customMessage = 'Are you sure you want to leave? Your points will not be saved.';
+    event.returnValue = customMessage; // Standard for most browsers
+    return customMessage; // For some older browsers
+}
 
 //Create eventlistener
 // Get the parent div
@@ -28,16 +41,27 @@ async function callBack() {
     const dataObject = await fetchData()
     const currentQuestionuestionNumber = dataObject.current_question_nr;
 
+    if(q_nr != currentQuestionuestionNumber) {
+        leaderBoardCreated = false;
+        timerStarted = false;
+        displayLeaderBoard(dataObject.users);
+        q_nr = currentQuestionuestionNumber;
+    }
+
     if (dataObject.quiz[currentQuestionuestionNumber] === "start") {
         document.getElementById("feedback").textContent = "Väntar på att spelet ska starta";
+        return;
     }
 
     if (dataObject.quiz[currentQuestionuestionNumber] === "end") {
         document.getElementById("feedback").textContent = "Quizet är slut";
         clearInterval(intervalId);
+        return;
     }
 
-    if (currentQuestionuestionNumber > OldQuestionNumber) {
+    if (currentQuestionuestionNumber == q_nr) {
+        displayLeaderBoard(dataObject.users);
+        
         document.getElementById("feedback").textContent = dataObject.quiz[currentQuestionuestionNumber].question;
 
         let usersContainer = document.querySelector("#users");
@@ -47,7 +71,8 @@ async function callBack() {
         // Iterate through userArray and update the content of <p> elements
         if (userArray.includes(username)) {
             usersContainer.innerHTML = "Du ska spela"
-        } else {
+        } 
+        else {
             userArray.forEach((user) => {
                 const button = document.createElement("button");
                 button.classList.add("voteBtn");
@@ -56,12 +81,45 @@ async function callBack() {
                 button.addEventListener("click", voteForPlayer)
                 usersContainer.append(button);
             });
-            startTimer();
-            OldQuestionNumber++;
+    
+            if(!timerStarted) {
+                startTimer();
+                timerStarted = true;
+            }
         }
     }
 }
 
+function displayLeaderBoard(users) {
+    if(!leaderBoardCreated) {
+        leaderBoardCreated = true;
+        let number = 1;
+        users.sort((a, b) => b.points - a.points);
+    
+        let leaderBoard = document.createElement("div");
+    
+        users.forEach ((user) => {
+            let p = `<p>${number}. <b style="background-color: ${user.color}">${user.username}</b>, Points: ${user.points}</p>`
+            leaderBoard.innerHTML += p;
+            number++;
+        })
+    
+        document.querySelector("body").append(leaderBoard);
+        let main = document.querySelector("main");
+        main.classList.add("hidden");
+    
+        let second = 0
+        leaderBoardIntervalId = setInterval(() => {
+            if (second === 5) {
+                clearInterval(leaderBoardIntervalId);
+                leaderBoard.remove();
+                main.classList.remove("hidden");
+            }
+    
+            second++;
+        }, interval);
+    }
+}
 
 function startTimer() {
     let second = 0
@@ -72,7 +130,7 @@ function startTimer() {
                 btn.setAttribute("disabled", true);
             })
         }
-        console.log(second);
+
         second++;
     }, interval);
 }
