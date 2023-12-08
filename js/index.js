1687e74a6ae758f10059d01a89276465b45c5493
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2023-12-08 14:49:28 +0100

    startpage css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 "use strict";
+
+document.getElementById("info").addEventListener("click", showInfo);
+
+function showInfo() {
+    console.log("hej kajsa");
+    let infoBox = document.getElementById("infoBox");
+    infoBox.classList.toggle("show");
+}
